200d57b44c1af177f0003ef4643433507bb6f34c
chiehen <47825654+chiehen@users.noreply.github.com>
2019-07-11 14:51:16 +0800

    sidenav_complete

--- universal/universal.js
+++ universal/universal.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+        
+        var position = $($(this).attr("href")).offset().top - 100;
+        console.log(position);
+        $("body, html").animate({
+            scrollTop: position
+        }, 200);
+    });
 });
