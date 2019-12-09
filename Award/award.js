ee2a3578fcb055dc62e600bf21fe724514d98efa
chiehen <47825654+chiehen@users.noreply.github.com>
2019-12-09 17:16:41 +0800

    jquery done

--- Award/award.js
+++ Award/award.js
@@ -2,19 +2,20 @@ $( document ).ready(function() {
     $("a[href='#presentation']").click(function(e){
         e.preventDefault();
         console.log("in")
+        $("video.present").removeClass("d-none");
+        $(".dimmer").removeClass("d-none");
+        $("body").addClass("blurring");
+    });
+    $("a[href='#poster']").click(function(e){
+        e.preventDefault();
+        console.log("in")
+        $("img.poster").removeClass("d-none");
         $(".dimmer").removeClass("d-none");
         $("body").addClass("blurring");
     });
     $(".dimmer").click(function(){
         closeDimmer();
     });
-    /*
-    $("button.dimmer-close").click(function(e){
-        console.log("close")
-        e.preventDefault();
-        closeDimmer();
-    });
-    */
     $(".card").click(function(){
         event.stopPropagation();
     });
@@ -28,5 +29,12 @@ $( document ).ready(function() {
 
 function closeDimmer() {
     $(".dimmer").addClass("d-none");
-    $("body").removeClass("blurring");    
+    $("body").removeClass("blurring");  
+    if($("video.present").hasClass("d-none")) {
+        $("img.poster").addClass("d-none");
+        console.log("poster d-none")
+    }else{
+        $("video.present").addClass("d-none");
+        console.log("present d-none")
+    }
 }
